c9020b2b5ebf2e26ec6be048b5fb8a759ad85785
AnnaChernova42 <anna.chernova42@gmail.com>
2020-12-13 11:15:43 +0300

    Update dropDown.java

--- src/main/four/dropDown.java
+++ src/main/four/dropDown.java
@@ -1,46 +1,100 @@
-package main.four;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
-public class dropDown {
 
-	public static void main(String[] args) {
-    	System.setProperty("webdriver.chrome.driver", "./chromedriver75.exe");
+
+public class DropDown {
+
+	public static void main(String[] args) throws InterruptedException {
+    	System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
     	WebDriver driver = new ChromeDriver();
-    	driver.navigate().to("https://www.facebook.com");
-		// - валюта с select - статичный. выбор количества пассажиров - нет. Infant вообще плавающий
+/*
+    	// РћРєРЅР°	   				
+     	driver.navigate().to("https://www.globalsqa.com/demo-site/frames-and-windows/#Open%20New%20Window");   	
+    	Thread.sleep(3000);
+    	WebElement tab = driver.findElement(By.xpath("//h1[contains(text(),'Frames And Windows')]"));
+    	
+    	//РїРµСЂРµРјРµСЃС‚РёС‚СЊСЃСЏ Рє Р·Р°РіР»Р°РІРёСЋ (С‚РµРєСЃС‚) РѕРєРЅР°
+    	Actions action = new Actions(driver);
+    	action.moveToElement(tab).perform();
+    	
+    	tab = driver.findElement(By.xpath("//li[@id='Open New Window']"));
+    	tab.click();
+    	driver.findElement(By.xpath("//div[@class='single_tab_div resp-tab-content resp-tab-content-active']//a[@class='button e.g. button_hilite button_pale small_button'][contains(text(),'Click Here')]")).click();
+    	
+    	//РїРѕР»СѓС‡РёС‚СЊ СЃРїРёСЃРѕРє РІСЃРµС… РѕРєРѕРЅ
+    	Thread.sleep(3000);
+    	System.out.println(driver.getWindowHandle());
+		Object[] a = driver.getWindowHandles().toArray();
+		
+		System.out.println("Р’СЃРµ РѕРєРЅР°: ");
+		for (int i=0;i<a.length;i++) {
+			System.out.println("РћРєРЅРѕ " + i + ": " + a[i].toString());
+		}
+		
+		
+		driver.switchTo().window(a[1].toString());		
+		System.out.println("С‚РµРєСѓС‰РµРµ РѕРєРЅРѕ : "+driver.getWindowHandle());
+    	
+*/
+		
+//РћРџРћР’Р•Р©Р•РќРРЇ
+		
+		driver.navigate().to("https://www.seleniumeasy.com/test/bootstrap-alert-messages-demo.html");
+		driver.findElement(By.xpath("//button[@id='autoclosable-btn-success']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//button[@id='normal-btn-warning']")).click();
+		Thread.sleep(1000);
+		
+		driver.findElement(By.xpath("//div[@class='alert alert-warning alert-normal-warning']/button")).click();
+		
+		/*
+		Alert MyAlert=driver.switchTo().alert();
+		//РџРѕР»СѓС‡РёС‚СЊ С‚РµРєСЃС‚ РѕРїРѕРІРµС‰РµРЅРёСЏ
+				String Text = driver.switchTo().alert().getText();
+				System.out.println(Text);
+				*/
+//РќР°Р¶Р°С‚СЊ OK
+	//	driver.switchTo().alert().accept();
+		
+//РќР°Р¶Р°С‚СЊ РћС‚РјРµРЅР°
+//		driver.switchTo().alert().dismiss();
+		
+/*
+//РњРѕРґР°Р»СЊРЅС‹Рµ РѕРєРЅР° 
+		driver.navigate().to("https://www.seleniumeasy.com/test/bootstrap-modal-demo.html");
+		driver.findElement(By.xpath("//body/div[@class='container-fluid text-center']/div[@class='row']/div[@class='col-md-6 text-left']/div[1]/div[1]/div[1]/div[2]/a[1]")).click();
+	//	driver.switchTo().frame(driver.findElement(By.id("myModal0")));
+		driver.switchTo().activeElement();
+		
+		System.out.println(driver.getWindowHandles().toString());
 
-    	//Выпадающие списки static
-    			Select dropdown = new Select(driver.findElement(By.id("day")));
-    			dropdown.selectByIndex(3);
-    			dropdown.selectByValue("12");
-    			dropdown.selectByVisibleText("День");
-    			// оборатите внимание на значение по умолчанию
-    			// запишите выбор своего дня рождения по value
-    			// запишите выбор дня рождения соседа по любому другому методу
-    		//	dropdown.deselectAll();   // тут не работает 
-    			
-    	// сложные выпадашки		
-    			driver.navigate().to("https://www.booking.com/");
-    			driver.findElement(By.id("xp__guests__toggle")).click();
-    			driver.findElement(By.xpath("//span[@class='bui-button__text']")).click();
-    			int i=1;
-    			String str=null;
-    			while(i<3) {
-    				driver.findElement(By.xpath("//div[contains(@class,'sb-group__field sb-group-children')]//span[@class='bui-button__text'][contains(text(),'+')]")).click();
-    				str="//select[@aria-label='Возраст ребенка "+i+"']";
-    				System.out.println(str);
-    				dropdown = new Select(driver.findElement(By.xpath(str)));
-    				dropdown.selectByValue(Integer.toString(i));
-    				i++;
-    			}
-    			for (int j=1;j<2;j++) {
-    				driver.findElement(By.xpath("//div[contains(@class,'sb-group__field sb-group__field-rooms')]//span[@class='bui-button__text'][contains(text(),'+')]")).click();
-    			}
-    			
-    			// Dynamic	- значения подгружаются только после выбора предыдущего значения
+		
+//РћС‚РїСЂР°РІРёС‚СЊ С‚РµРєСЃС‚ РІ РїРѕР»Рµ РІРІРѕРґР°
+		driver.switchTo().alert().sendKeys("test");
+		
+		
+		
+//РњРѕРґР°Р»СЊРЅС‹Рµ РѕРєРЅР° 
+		driver.switchTo().frame(driver.findElement(By.id("ModalId")));
+		driver.switchTo().activeElement();
+		
+//Drag&Drop
+  driver.navigate().to("http://demo.guru99.com/test/drag_drop.html");
+  WebElement sum = driver.findElement(By.xpath("//section[@id='g-container-main']//li[4]//a[1]"));
+  
+		WebElement From=driver.findElement(By.xpath("//*[@id='forth'][2]/a"));
+		WebElement To=driver.findElement(By.xpath("//*[@id='bank']/li"));; 
+		Actions action=new Actions(driver);
+		Action dragdrop = action.clickAndHold(From).moveToElement(To).release(To).build();
+		dragdrop.perform();
+	*/    			
+
 	}
 
 }
